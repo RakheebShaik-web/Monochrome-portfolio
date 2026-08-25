4921dd3efe0e12c7709aef1693d8f2087bf1fa74
RAKHEEB SHAIK <shaikrakheeb280@gmail.com>
2026-08-26 04:36:55 +0530

    Restore verified monochrome portfolio

--- build.mjs
+++ build.mjs
@@ -2,5 +2,5 @@ import { cp, mkdir, rm } from 'node:fs/promises';
 const output = new URL('./public/', import.meta.url);
 await rm(output, { recursive: true, force: true });
 await mkdir(output, { recursive: true });
-await Promise.all(['index.html', 'styles.css', 'script.js'].map(file => cp(new URL(file, import.meta.url), new URL(file, output))));
+await Promise.all(['index.html', 'styles.css', 'interactions.css', 'script.js'].map(file => cp(new URL(file, import.meta.url), new URL(file, output))));
 console.log('Production build written to public/');
